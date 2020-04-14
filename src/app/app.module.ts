55ddb54d87dbe0d89f381d8e8d07f9f3ed92d048
Anuj Parmar <anujparmar545@gmail.com>
2020-04-15 00:10:01 +0530

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,9 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+import { LoginComponent } from './login/login.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 @NgModule({
@@ -34,7 +37,8 @@ import { LeaderService } from './services/leader.service';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -48,8 +52,11 @@ import { LeaderService } from './services/leader.service';
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
+
+    MatDialogModule,
     
   ],
+  entryComponents: [ LoginComponent ],
   providers: [DishdetailComponent, DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent]
 })
